e20b6ba51645113a5aaf86071c7377b45d718637
Ayumu Ishihara <a-ishihara@coel-inc.jp>
2026-06-18 16:00:30 +0900

    "コードフォーマットの修正

--- CS_Actions_WebApp.Tests/Service/CalcServiceTests.cs
+++ CS_Actions_WebApp.Tests/Service/CalcServiceTests.cs
@@ -1,5 +1,6 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CS_Actions_WebApp.Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CS_Actions_WebApp.Tests.Services;
 /// <summary>
 /// 計算機能を提供するサービスクラスの単体テストドライバ
@@ -12,12 +13,12 @@ public class CalcServiceTests
     /// </summary>   
     [TestMethod]
     public void Execute_Addition_ReturnsCorrectValue()
-{
-    var service = new CalcService();
-    var result = service.Execute(10, 5, "+");
-    // テストを成功させる
-    Assert.AreEqual(15, result);
-}
+    {
+        var service = new CalcService();
+        var result = service.Execute(10, 5, "+");
+        // テストを成功させる
+        Assert.AreEqual(15, result);
+    }
     /// <summary>
     /// ゼロ除算処理のテスト
     /// </summary>
